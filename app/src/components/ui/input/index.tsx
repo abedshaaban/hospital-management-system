f8b241a18eb4bd99ff7a6be5f3cebfda2bb014e8
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2023-12-03 13:49:01 +0200

    [ui]: created sign in form

--- app/src/components/ui/input/index.tsx
+++ app/src/components/ui/input/index.tsx
@@ -4,24 +4,39 @@ import './index.css'
 
 type Props = {
   type: HTMLInputTypeAttribute
+  label: string
   placeholder: string
-  className?: string
   value: any
   setFunc: (val: any) => void
+  className?: string
+  required?: boolean
 }
 
-export default function Input({ type, placeholder, value, setFunc, className }: Props) {
+export default function Input({
+  type,
+  label,
+  placeholder,
+  value,
+  setFunc,
+  className,
+  required
+}: Props) {
   function handleChange(e: ChangeEvent<HTMLInputElement>) {
     setFunc(e.target.value)
   }
 
   return (
-    <input
-      className={`ui-input ${className}`}
-      type={type}
-      value={value}
-      placeholder={placeholder}
-      onChange={handleChange}
-    />
+    <div className="ui-get-values">
+      <label>{label}</label>
+
+      <input
+        className={`ui-input ${className}`}
+        type={type}
+        value={value}
+        placeholder={placeholder}
+        onChange={handleChange}
+        required={required}
+      />
+    </div>
   )
 }
--- app/src/pages/home/index.tsx
+++ app/src/pages/home/index.tsx
@@ -1,3 +1,67 @@
+import Input from '@/components/ui/input'
+
+import './index.css'
+
+import { useState } from 'react'
+
 export default function Home() {
-  return <div>Home</div>
+  const [credentials, setCredentials] = useState({
+    email: '',
+    pwd: ''
+  })
+
+  const [signIn, setSignIn] = useState(false)
+
+  function handleFormChange() {
+    setSignIn(!signIn)
+  }
+
+  return (
+    <div className="home">
+      <form className="home-form">
+        <div className="home-form-header flex-row">
+          <h2
+            className={`${signIn && 'home-form-header-not-active'}`}
+            onClick={handleFormChange}
+          >
+            Register
+          </h2>
+          <h2
+            className={`${!signIn && 'home-form-header-not-active'}`}
+            onClick={handleFormChange}
+          >
+            Sign in
+          </h2>
+        </div>
+
+        <div className="home-form-body flex-col">
+          <Input
+            type={'email'}
+            label={'Email:'}
+            placeholder={'mohamad@gmail.com'}
+            value={credentials.email}
+            setFunc={(val) => {
+              setCredentials({ ...credentials, email: val })
+            }}
+            required
+          />
+
+          <Input
+            type={'password'}
+            label={'Password:'}
+            placeholder={'*********'}
+            value={credentials.pwd}
+            setFunc={(val) => {
+              setCredentials({ ...credentials, pwd: val })
+            }}
+            required
+          />
+        </div>
+
+        <div className="home-form-footer center">
+          <button type="submit">Sign in</button>
+        </div>
+      </form>
+    </div>
+  )
 }
