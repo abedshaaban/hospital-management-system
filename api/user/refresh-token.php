7425c84e1d866b8b552c6660a00fd300eb68eec1
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2023-12-04 22:36:31 +0200

    [clean-up]: split php code into a util file

--- api/user/refresh-token.php
+++ api/user/refresh-token.php
@@ -1,34 +1,11 @@
 <?php
-require dirname(__DIR__) . '/vendor/autoload.php';
-
-use Firebase\JWT\JWT;
-use Firebase\JWT\Key;
-
 
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 
 include("../connection.php");
-
-$request_body = file_get_contents("php://input");
-$data = json_decode($request_body, true);
-
-$key = "secret_key_here";
-$alg = "HS256";
-$user_jwt = $_POST["token"] ?? $data["token"];
-
-$decoded_res = [];
-
-try {
-    $decoded = JWT::decode($user_jwt, new key($key, $alg));
-    $decoded_res["status"] = true;
-    $decoded_res["data"] = $decoded;
-} catch (Exception $e) {
-    $decoded_res["status"] = false;
-    $decoded_res["error"]=  $e->getMessage();
-}
-
+include("./util.php");
 
 if ($decoded_res["status"] === true) {
     $q = $mysqli->prepare("select
