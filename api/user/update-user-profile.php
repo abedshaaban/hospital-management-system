b7c6b62dd37b454c34f5b4160674575f03071ee8
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2023-12-05 16:32:45 +0200

    [core]; update user inside table

--- api/user/update-user-profile.php
+++ api/user/update-user-profile.php
@@ -11,10 +11,38 @@ include("./util.php");
 $request_body = file_get_contents("php://input");
 $data = json_decode($request_body, true);
 
-$user_profile = $_POST["user"] ?? $data["user"];
+$first_name_to_be = $_POST["first_name"] ?? $data["first_name"];
+$last_name_to_be = $_POST["last_name"] ?? $data["last_name"];
+$birth_date_to_be = $_POST["birth_date"] ?? $data["birth_date"];
 
 if ($response["status"] === true) {
-    echo $user_profile;
+
+    $q = $mysqli->prepare("
+    UPDATE
+        `users`
+    SET
+        `first_name` = ?,
+        `last_name` = ?,
+        `birth_date` = ?
+    WHERE
+        `uuid` = ?
+    ");
+
+    $q->bind_param(
+        "sssss",
+        $email_to_be,
+        $first_name_to_be,
+        $last_name_to_be,
+        $birth_date_to_be,
+        $decoded_res["data"]->uuid
+    );
+    $q->execute();
+
+    $response["data"]["first_name"] = $first_name_to_be;
+    $response["data"]["last_name"] = $last_name_to_be;
+    $response["data"]["birth_date"] = $birth_date_to_be;
+
+    echo json_encode($response);
 
 }else{   
     echo json_encode($response);
